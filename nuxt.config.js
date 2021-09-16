6f6160562faafcb870c32ac2a4955c98a82b4ab8
Barış Taş <barisizmist@gmail.com>
2021-09-16 16:44:23 +0300

    static

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  // target: 'static',
+  target: 'static',
   head: {
     titleTemplate: '%s - jamstack-nuxt',
     title: 'jamstack-nuxt',
