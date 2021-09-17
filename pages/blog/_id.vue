015dac66123d7e0e9b5a9f30eff24df26eb3a23d
Barış Taş <barisizmist@gmail.com>
2021-09-17 17:29:04 +0300

    post id

---
+++ pages/blog/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ post.title }}</h2>
+    <div>{{ post.content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const post = await fetch(`http://localhost:1337/posts/${params.id}`).then(
+      (res) => res.json()
+    )
+    return { post }
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <NuxtLink :to="`blog/${post.id}`">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData() {
+    const posts = await fetch('http://localhost:1337/posts').then((res) =>
+      res.json()
+    )
+    return { posts }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
